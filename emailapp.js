f3881d1437e71710096fc8654a8a67b071c07e2d
arjunsatish <35654774+arjunsatish@users.noreply.github.com>
2018-03-01 18:19:26 +0530

    Add files via upload

---
+++ emailapp.js
@@ -0,0 +1,52 @@
+
+            var name = " ";
+            var imgurl = " ";
+            var email = " ";
+
+        function onSignIn(googleUser){
+                var profile = googleUser.getBasicProfile();
+                $(".g-signin2").css("display","none");
+                $(".success").css("display","block");
+                $(".profiledetails").css("display","none");
+                $(".addressbook").css("display","none");
+
+                name=profile.getName();
+                imgurl = profile.getImageUrl();
+                email = profile.getEmail();
+        };
+
+        function ProfileDetails(){
+          $(".g-signin2").css("display","none");
+          $(".success").css("display","none");
+          $(".profiledetails").css("display","block");
+          $(".addressbook").css("display","none");
+
+          $("#name").text(name);
+          $("#pic").attr('src',imgurl);
+          $("#email").text(email);
+        };
+
+        function AddressBook(){
+          $(".g-signin2").css("display","none");
+          $(".success").css("display","none");
+          $(".profiledetails").css("display","none");
+          $(".addressbook").css("display","block");
+
+        };
+
+        function gohome(){
+
+          $(".g-signin2").css("display","none");
+          $(".success").css("display","block");
+          $(".profiledetails").css("display","none");
+          $(".addressbook").css("display","none");
+
+        };
+
+
+        function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+            console.log('User signed out.');
+            });
+        };
