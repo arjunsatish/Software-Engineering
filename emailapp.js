ca8c3b0a268615565481c8737b3a60c72b547e24
arjunsatish <35654774+arjunsatish@users.noreply.github.com>
2018-03-17 15:02:43 +0530

    Add files via upload

---
+++ emailapp.js
@@ -0,0 +1,66 @@
+
+            var name = " ";
+            var imgurl = " ";
+            var email = " ";
+
+        function onSignIn(googleUser){
+                var profile = googleUser.getBasicProfile();
+                $(".g-signin2").css("display","none");
+                $(".success").css("display","block");
+                $(".profiledetails").css("display","none");
+                $(".addressbook").css("display", "none");
+                $(".aboutUs").css("display", "none");
+                $(".signout").css("display", "block");
+
+                name=profile.getName();
+                imgurl = profile.getImageUrl();
+                email = profile.getEmail();
+        };
+
+        function aboutUs() {
+            $(".g-signin2").css("display", "none");
+            $(".success").css("display", "none");
+            $(".profiledetails").css("display", "none");
+            $(".addressbook").css("display", "none");
+            $(".aboutUs").css("display", "block");
+            $(".signout").css("display", "block");
+        };
+        
+        function ProfileDetails(){
+          $(".g-signin2").css("display","none");
+          $(".success").css("display","none");
+          $(".profiledetails").css("display","block");
+            $(".addressbook").css("display", "none");
+            $(".aboutUs").css("display", "none");
+            $(".signout").css("display", "block");
+          $("#name").text(name);
+          $("#pic").attr('src',imgurl);
+          $("#email").text(email);
+        };
+
+        function AddressBook(){
+          $(".g-signin2").css("display","none");
+          $(".success").css("display","none");
+          $(".profiledetails").css("display","none");
+            $(".addressbook").css("display", "block");
+            $(".aboutUs").css("display", "none");
+            $(".signout").css("display", "block");
+        };
+
+        function gohome(){
+
+          $(".g-signin2").css("display","none");
+          $(".success").css("display","block");
+          $(".profiledetails").css("display","none");
+            $(".addressbook").css("display", "none");
+            $(".aboutUs").css("display", "none");
+            $(".signout").css("display", "block");
+        };
+
+
+        function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+            console.log('User signed out.');
+            });
+        };
